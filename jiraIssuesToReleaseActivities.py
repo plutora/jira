cdc7cef5401c63c54af0b076eaf9a4bd0d7e6fb5
Greg Maxey <greg.maxey@plutora.com>
2017-07-10 09:11:16 -0400

    Mostly documentation, added Jira URL to file

--- jiraIssuesToReleaseActivities.py
+++ jiraIssuesToReleaseActivities.py
@@ -1,8 +1,34 @@
 # python 2.7
 # Create Plutora Release Activities from Jira Tasks
-# To use, you'll need to provide valid Jira crendials and a credentials.cfg file for the Plutor login
-# Also, make sure to reference a valid Release and Release Phase on lines 21 and 23
+# To use, you'll need to provide a valid Jira crendials file (jira.cfg): 
+#	{
+#		"username":"JiraLoginName",
+#		"password":"JiraLoginPassword"
+#	}
+# and a credentials.cfg file for the Plutor login:
+#	{
+#	  "urls":{
+#		"authUrl":"https://usoauth.plutora.com/",
+#		"baseUrl":"https://usapi.plutora.com/"
+#	  },
+#	  "credentials": {
+#		"client_id":"XXXXXXXXXXXXXXXXXXXXXXXXXX",
+#		"client_secret":"YYYYYYYYYYYYYYYYYYYYYYYYYY",
+#		"username":"PlutoraLoginName",
+#		"password":"PlutoraLoginPassword"
+#		}
+#	}
+# Where usoath and usapi should be replaced by ukoath and ukapi is you're
+# using a UK-based Plutora instance, and auoath and uaapi if in Asia.
+# Set a valid Release name and one of its Phase names below:
+releaseName = 'Test add issues from Jira'
+releasePhase = "DEV"
+# Identify an existing Release stakeholder to assign the Activities, for example:
+releaseStakeholder = 'greg.maxey@plutora.com'
+# Create a Jira filter to find the desired tasks, for example:
+jql_str = 'type = task and duedate>"2017-01-01"'
 
+#---------------END of instructions--------------------------------------
 
 # pip install jira
 from jira import JIRA
@@ -16,27 +42,28 @@ import json
 # Jira Credentials - load from jira.cfg
 with open("jira.cfg") as data_file:
 	jiraCfg = json.load(data_file)
+j_url = jiraCfg["url"]
 j_user = jiraCfg["username"]
 j_password = jiraCfg["password"]
 
 # Login to Plutora Jira instance
-jira = JIRA(server='https://plutora.atlassian.net',basic_auth=(j_user,j_password))
+jira = JIRA(server=j_url,basic_auth=(j_user,j_password))
 
 # Select Release to apply the tasks/Activities to
-releaseId = plutora.guidByPathAndName('releases','Test add issues from Jira')
+releaseId = plutora.guidByPathAndName('releases',releaseName)
 # Select target Release Phase for the Activities
-DEV_phaseId = plutora.guidByPathAndName('workitemnames/phases','DEV')
+DEV_phaseId = plutora.guidByPathAndName('workitemnames/phases',releasePhase)
 
 # Retrieve Jira tasks
-jql_str = 'type = task and duedate>"2017-01-01"'
 tasks = jira.search_issues(jql_str, startAt=0, maxResults=10, validate_query=True,
               fields=None, expand=None, json_result=None)
+print "Found %d Jira tasks." % len(tasks)
 # For each Jira task found
 for task in tasks:
-	print "Processing Jira case %s" % jira.issue(task.key)
+	print "Processing Jira task %s" % jira.issue(task.key)
 	taskHandle = jira.issue(task.id)
 	Title = taskHandle.raw['fields']['summary']
-	Description = "Jira case %s" % jira.issue(task.key)
+	Description = "Jira task %s" % jira.issue(task.key)
 	EndDate = taskHandle.raw['fields']['duedate']
 	# Only create Activities with due dates
 	if (EndDate==None):
@@ -49,7 +76,7 @@ for task in tasks:
 		'ActivityDependencyType': 'None',
 		'Type': 'Activity',
 		'Status': 'NotStarted',
-		'AssignedToID': plutora.guidByPathAndName('users','greg.maxey@plutora.com',field="userName"),
+		'AssignedToID': plutora.guidByPathAndName('users',releaseStakeholder,field="userName"),
 		'AssignedWorkItemID': plutora.guidByPathAndName("releases/%s/phases" % releaseId, DEV_phaseId, field="workItemNameID"), # Phase
 		'EndDate': "%sT00:00:00" % EndDate
 	}
